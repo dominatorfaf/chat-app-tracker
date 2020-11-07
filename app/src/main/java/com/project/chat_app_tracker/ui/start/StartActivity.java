c6f02ab63bcb71178543519a7ee10e454dd836a4
dominatorfaf <dominicflocea.dev@gmail.com>
2020-11-08 00:06:40 +0200

    Changed the UI

--- app/src/main/java/com/project/chat_app_tracker/ui/start/StartActivity.java
+++ app/src/main/java/com/project/chat_app_tracker/ui/start/StartActivity.java
@@ -1,4 +1,4 @@
-package com.project.chat_app_tracker.ui.login;
+package com.project.chat_app_tracker.ui.start;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,12 +6,11 @@ import android.os.Bundle;
 
 import com.project.chat_app_tracker.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_start);
     }
 }
