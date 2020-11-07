5c70035d2c91f1e51a8b12c9b829305eb799c4cb
dominatorfaf <dominicflocea.dev@gmail.com>
2020-11-07 18:04:19 +0200

    first commit

---
+++ app/src/main/java/com/project/chat_app_tracker/ui/chat/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.project.chat_app_tracker.ui.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.chat_app_tracker.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
